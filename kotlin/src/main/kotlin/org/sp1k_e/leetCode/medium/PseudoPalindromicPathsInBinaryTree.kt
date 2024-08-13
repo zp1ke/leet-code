0a8ef8a103039035fae52443cd8a17b201c923e0
Michel Atucha <miche.atucha@gmail.com>
2024-08-13 09:30:26 -0500

    pseudo palindromic paths

--- kotlin/src/main/kotlin/org/sp1k_e/leetCode/medium/PseudoPalindromicPathsInBinaryTree.kt
+++ kotlin/src/main/kotlin/org/sp1k_e/leetCode/medium/PseudoPalindromicPathsInBinaryTree.kt
@@ -1,4 +1,13 @@
 package org.sp1k_e.leetCode.medium
 
+// https://leetcode.com/problems/pseudo-palindromic-paths-in-a-binary-tree/description/
 class PseudoPalindromicPathsInBinaryTree {
+    fun pseudoPalindromicPaths(root: TreeNode?): Int {
+        return 0
+    }
+}
+
+class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
 }
--- kotlin/src/test/kotlin/org/sp1k_e/leetCode/medium/PseudoPalindromicPathsInBinaryTreeTests.kt
+++ kotlin/src/test/kotlin/org/sp1k_e/leetCode/medium/PseudoPalindromicPathsInBinaryTreeTests.kt
@@ -1,4 +1,28 @@
 package org.sp1k_e.leetCode.medium
 
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
 class PseudoPalindromicPathsInBinaryTreeTests {
+    @Test
+    fun test() {
+        val testCases = arrayOf(
+            PseudoPalindromicPathsInBinaryTreeTestCase(1, arrayOf(9)),
+            PseudoPalindromicPathsInBinaryTreeTestCase(2, arrayOf(2, 3, 1, 3, 1, null, 1)),
+            PseudoPalindromicPathsInBinaryTreeTestCase(1, arrayOf(2, 1, 1, 1, 3, null, null, null, null, null, 1)),
+        )
+        val testObject = PseudoPalindromicPathsInBinaryTree()
+        for (testCase in testCases) {
+            val result = testObject.pseudoPalindromicPaths(testCase.treeNode)
+            assertEquals(testCase.expected, result)
+        }
+    }
+}
+
+private data class PseudoPalindromicPathsInBinaryTreeTestCase(val expected: Int, val input: Array<Int?>) {
+    val treeNode: TreeNode?
+        get() {
+            // TODO
+            return null
+        }
 }
