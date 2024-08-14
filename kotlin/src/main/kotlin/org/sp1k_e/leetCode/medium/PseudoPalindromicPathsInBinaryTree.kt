8adc284500d84c4a321ffbd9598ac61740fe48b7
Sp1ke <miche.atucha@gmail.com>
2024-08-13 23:13:34 -0500

    Pseudo-Palindromic Paths in a Binary Tree

--- kotlin/src/main/kotlin/org/sp1k_e/leetCode/medium/PseudoPalindromicPathsInBinaryTree.kt
+++ kotlin/src/main/kotlin/org/sp1k_e/leetCode/medium/PseudoPalindromicPathsInBinaryTree.kt
@@ -3,11 +3,39 @@ package org.sp1k_e.leetCode.medium
 // https://leetcode.com/problems/pseudo-palindromic-paths-in-a-binary-tree/description/
 class PseudoPalindromicPathsInBinaryTree {
     fun pseudoPalindromicPaths(root: TreeNode?): Int {
-        return 0
+        if (root == null) {
+            return 0
+        }
+        return paths(root)
+    }
+
+    private fun paths(root: TreeNode, odds: Set<Int> = emptySet()): Int {
+        val oddsList = odds.toMutableSet()
+        if (!oddsList.add(root.`val`)) {
+            oddsList.remove(root.`val`)
+        }
+        if (root.left == null && root.right == null) {
+            return if (oddsList.size <= 1) 1 else 0
+        }
+        val leftPaths = if (root.left != null) paths(root.left!!, oddsList) else 0
+        val rightPaths = if (root.right != null) paths(root.right!!, oddsList) else 0
+        return leftPaths + rightPaths
     }
 }
 
 class TreeNode(var `val`: Int) {
     var left: TreeNode? = null
     var right: TreeNode? = null
+
+    companion object {
+        fun create(index: Int, array: Array<Int?>): TreeNode? {
+            if (index > array.lastIndex || array[index] == null) {
+                return null
+            }
+            val node = TreeNode(`val` = array[index]!!)
+            node.left = create(2 * index + 1, array)
+            node.right = create(2 * index + 2, array)
+            return node
+        }
+    }
 }
--- kotlin/src/test/kotlin/org/sp1k_e/leetCode/medium/PseudoPalindromicPathsInBinaryTreeTests.kt
+++ kotlin/src/test/kotlin/org/sp1k_e/leetCode/medium/PseudoPalindromicPathsInBinaryTreeTests.kt
@@ -7,9 +7,10 @@ class PseudoPalindromicPathsInBinaryTreeTests {
     @Test
     fun test() {
         val testCases = arrayOf(
-            PseudoPalindromicPathsInBinaryTreeTestCase(1, arrayOf(9)),
             PseudoPalindromicPathsInBinaryTreeTestCase(2, arrayOf(2, 3, 1, 3, 1, null, 1)),
             PseudoPalindromicPathsInBinaryTreeTestCase(1, arrayOf(2, 1, 1, 1, 3, null, null, null, null, null, 1)),
+            PseudoPalindromicPathsInBinaryTreeTestCase(1, arrayOf(9)),
+            PseudoPalindromicPathsInBinaryTreeTestCase(1, arrayOf(9,5,4,5,null,2,6,2,5,null,8,3,9,2,3,1,1,null,4,5,4,2,2,6,4,null,null,1,7,null,5,4,7,null,null,7,null,1,5,6,1,null,null,null,null,9,2,null,9,7,2,1,null,null,null,6,null,null,null,null,null,null,null,null,null,5,null,null,3,null,null,null,8,null,1,null,null,8,null,null,null,null,2,null,8,7)),
         )
         val testObject = PseudoPalindromicPathsInBinaryTree()
         for (testCase in testCases) {
@@ -22,7 +23,9 @@ class PseudoPalindromicPathsInBinaryTreeTests {
 private data class PseudoPalindromicPathsInBinaryTreeTestCase(val expected: Int, val input: Array<Int?>) {
     val treeNode: TreeNode?
         get() {
-            // TODO
-            return null
+            if (input.isEmpty()) {
+                return null
+            }
+            return TreeNode.create(0, input)
         }
 }
