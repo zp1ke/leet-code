b7f859f8b90f1fcc36b1c1d696e608a3eacbafca
Michel Atucha <miche.atucha@gmail.com>
2024-08-21 12:14:32 -0500

    The k strongest values in an array

--- kotlin/src/main/kotlin/org/sp1k_e/leetCode/medium/KStrongestValuesInArray.kt
+++ kotlin/src/main/kotlin/org/sp1k_e/leetCode/medium/KStrongestValuesInArray.kt
@@ -5,22 +5,19 @@ import kotlin.math.absoluteValue
 // https://leetcode.com/problems/the-k-strongest-values-in-an-array/description/
 class KStrongestValuesInArray {
     fun getStrongest(arr: IntArray, k: Int): IntArray {
-        val median = arr.sortedArray().median
+        val sortedArray = arr.sortedArray()
+        val medianIndex = (sortedArray.size - 1) / 2
+        val median = sortedArray[medianIndex]
+
         val arrByStrongest = arr.toMutableList()
         arrByStrongest.sortWith { a, b ->
             val aValue = (a - median).absoluteValue
             val bValue = (b - median).absoluteValue
             when {
-                (aValue != bValue) -> aValue - bValue
-                else -> a - b
+                (aValue != bValue) -> bValue - aValue
+                else -> b - a
             }
         }
-        return arrByStrongest.reversed().subList(0, k).toIntArray()
+        return arrByStrongest.subList(0, k).toIntArray()
     }
 }
-
-private val IntArray.median: Int
-    get() {
-        val medianIndex = (size - 1) / 2
-        return this[medianIndex]
-    }
