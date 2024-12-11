bb32d8c983fb66e831d2ad3595ee1fe9dca41de1
Michel Atucha <zp1ke@outlook.com>
2024-12-11 08:26:06 -0500

    neetcode products-of-array-discluding-self

---
+++ kotlin/src/main/kotlin/org/sp1k_e/leetCode/medium/ProductsArrayDiscludingSelf.kt
@@ -0,0 +1,26 @@
+package org.sp1k_e.leetCode.medium
+
+// https://neetcode.io/problems/products-of-array-discluding-self
+class ProductsArrayDiscludingSelf {
+    fun productExceptSelf(nums: IntArray): IntArray {
+        val prefix = IntArray(nums.size)
+        var prev = 1
+        for (i in 0..nums.lastIndex) {
+            prefix[i] = prev
+            prev *= nums[i]
+        }
+
+        val suffix = IntArray(nums.size)
+        prev = 1
+        for (i in nums.lastIndex downTo 0) {
+            suffix[i] = prev
+            prev *= nums[i]
+        }
+
+        val result = IntArray(nums.size)
+        for (i in 0..nums.lastIndex) {
+            result[i] = prefix[i] * suffix[i]
+        }
+        return result
+    }
+}
---
+++ kotlin/src/test/kotlin/org/sp1k_e/leetCode/medium/ProductsArrayDiscludingSelfTests.kt
@@ -0,0 +1,30 @@
+package org.sp1k_e.leetCode.medium
+
+import kotlin.test.Test
+import kotlin.test.assertContentEquals
+
+class ProductsArrayDiscludingSelfTests {
+    @Test
+    fun test() {
+        val testCases = arrayOf(
+            ProductsArrayDiscludingSelfTestCase(
+                intArrayOf(48, 24, 12, 8),
+                input = intArrayOf(1, 2, 4, 6)
+            ),
+            ProductsArrayDiscludingSelfTestCase(
+                intArrayOf(0, -6, 0, 0, 0),
+                input = intArrayOf(-1, 0, 1, 2, 3)
+            ),
+        )
+        val testObject = ProductsArrayDiscludingSelf()
+        for (testCase in testCases) {
+            val result = testObject.productExceptSelf(testCase.input)
+            assertContentEquals(testCase.expected, result)
+        }
+    }
+}
+
+private data class ProductsArrayDiscludingSelfTestCase(
+    val expected: IntArray,
+    val input: IntArray
+)
