2ff3bbf4f80f521fb610e844957e627fc492b62c
Michel Atucha <zp1ke@outlook.com>
2024-12-12 08:48:15 -0500

    neetcode valid-sudoku

---
+++ kotlin/src/main/kotlin/org/sp1k_e/leetCode/medium/ValidSudoku.kt
@@ -0,0 +1,28 @@
+package org.sp1k_e.leetCode.medium
+
+// https://neetcode.io/problems/valid-sudoku
+class ValidSudoku {
+    fun isValidSudoku(board: Array<CharArray>): Boolean {
+        val sudokuSquareSize = 3
+        val sudokuSize = sudokuSquareSize * sudokuSquareSize
+        val rows = Array(sudokuSize) { mutableSetOf<Char>() }
+        val cols = Array(sudokuSize) { mutableSetOf<Char>() }
+        val squares = Array(sudokuSize) { mutableSetOf<Char>() }
+        for (rowIndex in 0..<sudokuSize) {
+            for (colIndex in 0..<sudokuSize) {
+                val value = board[rowIndex][colIndex]
+                if (value < '1' || value > '9') {
+                    continue
+                }
+                if (!rows[rowIndex].add(value) || !cols[colIndex].add(value)) {
+                    return false
+                }
+                val squareIndex = ((rowIndex / sudokuSquareSize) * sudokuSquareSize) + (colIndex / sudokuSquareSize)
+                if (!squares[squareIndex].add(value)) {
+                    return false
+                }
+            }
+        }
+        return true
+    }
+}
---
+++ kotlin/src/test/kotlin/org/sp1k_e/leetCode/medium/ValidSudokuTests.kt
@@ -0,0 +1,50 @@
+package org.sp1k_e.leetCode.medium
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ValidSudokuTests {
+    @Test
+    fun test() {
+        val testCases = arrayOf(
+            ValidSudokuTestCase(
+                true,
+                input = arrayOf(
+                    charArrayOf('1', '2', '.', '.', '3', '.', '.', '.', '.'),
+                    charArrayOf('4', '.', '.', '5', '.', '.', '.', '.', '.'),
+                    charArrayOf('.', '9', '8', '.', '.', '.', '.', '.', '3'),
+                    charArrayOf('5', '.', '.', '.', '6', '.', '.', '.', '4'),
+                    charArrayOf('.', '.', '.', '8', '.', '3', '.', '.', '5'),
+                    charArrayOf('7', '.', '.', '.', '2', '.', '.', '.', '6'),
+                    charArrayOf('.', '.', '.', '.', '.', '.', '2', '.', '.'),
+                    charArrayOf('.', '.', '.', '4', '1', '9', '.', '.', '8'),
+                    charArrayOf('.', '.', '.', '.', '8', '.', '.', '7', '9')
+                ),
+            ),
+            ValidSudokuTestCase(
+                false,
+                input = arrayOf(
+                    charArrayOf('1', '2', '.', '.', '3', '.', '.', '.', '.'),
+                    charArrayOf('4', '.', '.', '5', '.', '.', '.', '.', '.'),
+                    charArrayOf('.', '9', '1', '.', '.', '.', '.', '.', '3'),
+                    charArrayOf('5', '.', '.', '.', '6', '.', '.', '.', '4'),
+                    charArrayOf('.', '.', '.', '8', '.', '3', '.', '.', '5'),
+                    charArrayOf('7', '.', '.', '.', '2', '.', '.', '.', '6'),
+                    charArrayOf('.', '.', '.', '.', '.', '.', '2', '.', '.'),
+                    charArrayOf('.', '.', '.', '4', '1', '9', '.', '.', '8'),
+                    charArrayOf('.', '.', '.', '.', '8', '.', '.', '7', '9')
+                ),
+            )
+        )
+        val testObject = ValidSudoku()
+        for (testCase in testCases) {
+            val result = testObject.isValidSudoku(testCase.input)
+            assertEquals(testCase.expected, result)
+        }
+    }
+}
+
+private data class ValidSudokuTestCase(
+    val expected: Boolean,
+    val input: Array<CharArray>
+)
